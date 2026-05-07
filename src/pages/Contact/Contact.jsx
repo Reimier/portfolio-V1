312f11847f96fff489958797ae5d96a713d49473
Rei Seyer <reimierreyes0221@gmail.com>
2026-05-07 18:36:32 +0800

    Commit

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -46,14 +46,21 @@ export default function Contact(){
 
             <p id="header">Want to work together? feel free to say hi!</p>
             <p id="contact-desc">Send me a message so that we can work together on a 
-project or just say hi to me! Hit me up and I’ll get back to
-you as soon as possible.</p>
+                                  project or just say hi to me! Hit me up and I’ll get back to
+                                  you as soon as possible.</p>
 
         </div>
 
         <div className="contact-page" data-aos="fade-up">
 
         <form className="contact-form" onSubmit={sendEmail}>
+
+            <div className="socials-icon">
+            <a href="https://github.com/reimier" target="_blank" rel="noreferrer" className="icons"> <i class="fa-brands fa-github"></i> </a>
+            <a href="https://www.instagram.com/ier_reyes/?hl=en" target="_blank" rel="noreferrer" className="icons"> <i class="fa-brands fa-instagram"></i> </a>
+            <a href="https://www.linkedin.com/in/reimier-reyes-380a67355/" target="_blank" rel="noreferrer" className="icons"> <i class="fa-brands fa-linkedin-in"></i></a>
+            </div>
+
             <label>Name</label>
             <input placeholder="Enter your name" name="name" type="text" required />
 
