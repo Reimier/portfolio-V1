e8c2b6aa379c6752d8865d60ff6d3a8361429800
Rei Seyer <reimierreyes0221@gmail.com>
2026-07-24 09:02:11 +0800

    fix: update portfolio site

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -10,7 +10,16 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import Footer from "../../components/Footer/Footer";
 
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 export default function About(){
+
+    AOS.init({
+        duration: 800, // animation duration in ms
+        easing: 'ease-in-out',
+        once: true,    // whether animation should happen only once while scrolling down
+    });
 
     return(
         <>
@@ -60,44 +69,44 @@ export default function About(){
 
             </div>
 
-                <div className="about-exp">
+                <div className="about-exp" data-aos="fade-up">
 
-                <p className="about-titles"><i class="fa-solid fa-suitcase"></i> Work Experience</p>
+                <p data-aos="fade-up" className="about-titles"><i class="fa-solid fa-suitcase"></i> Work Experience</p>
 
-                <p className="exp-sub">Salesforce – Virtual Internship | 2025</p>
+                <p data-aos="fade-up" className="exp-sub">Salesforce – Virtual Internship | 2025</p>
 
-                <p className="exp-desc" >Successfully completed and collected numerous Superbadges in Salesforce Trailhead, demonstrating proficiency in key platform functionalities.</p>
+                <p data-aos="fade-up" className="exp-desc" >Successfully completed and collected numerous Superbadges in Salesforce Trailhead, demonstrating proficiency in key platform functionalities.</p>
 
-                <p className="exp-sub" >Calumpit MTO – Work Immersion Internship | 2023</p>
+                <p data-aos="fade-up" className="exp-sub" >Calumpit MTO – Work Immersion Internship | 2023</p>
 
-                <p className="exp-desc" >Streamlined organizational flow by arranging and archiving multiple paperwork, receipts, and related documents, resulting in an organized workplace.</p>
+                <p data-aos="fade-up" className="exp-desc" >Streamlined organizational flow by arranging and archiving multiple paperwork, receipts, and related documents, resulting in an organized workplace.</p>
 
             </div>
 
-            <div className="about-educ">
+            <div data-aos="fade-up" className="about-educ">
 
-                <p className="about-titles"  ><i class="fa-solid fa-graduation-cap"></i> Education</p>
+                <p data-aos="fade-up" className="about-titles"  ><i class="fa-solid fa-graduation-cap"></i> Education</p>
 
-                <p className="educ-sub"  >Bulacan State University - Malolos Campus | 2027</p>
+                <p data-aos="fade-up" className="educ-sub"  >Bulacan State University - Malolos Campus | 2027</p>
 
-                <p className="educ-desc"  >Bachelor of Science in Information Technology (BSIT). Specializes in Web and Mobile App Development.</p>
+                <p data-aos="fade-up" className="educ-desc"  >Bachelor of Science in Information Technology (BSIT). Specializes in Web and Mobile App Development.</p>
                 
                 {/* <p className="educ-sub" >FreeCodeCamp | 2026</p>
 
                 <p className="educ-desc"  >Responsive Web Design Certification & Front-End Development Libraries Certification (In progress).</p> */}
 
-                <p className="educ-sub" >Technical Education and Skills Development Authority | 2025</p>
+                <p data-aos="fade-up" className="educ-sub" >Technical Education and Skills Development Authority | 2025</p>
 
-                <p className="educ-desc"  >TESDA Online Program (TOP). Certificate of Completion on Microsoft Digital Literacy, Computer System Servicing, and SMART Technopreneurship 101.</p>
+                <p data-aos="fade-up" className="educ-desc"  >TESDA Online Program (TOP). Certificate of Completion on Microsoft Digital Literacy, Computer System Servicing, and SMART Technopreneurship 101.</p>
 
 
             </div>
 
-            <div className="about-tech">
+            <div data-aos="fade-up" className="about-tech">
 
-                <p className="about-titles" ><i class="fa-solid fa-layer-group"></i> Tech Stack</p>
+                <p data-aos="fade-up" className="about-titles" ><i class="fa-solid fa-layer-group"></i> Tech Stack</p>
 
-                <p className="tech-sub" >
+                <p data-aos="fade-up" className="tech-sub" >
                     {/* <i className="fa-brands fa-react"></i>
                     <i className="fa-brands fa-node-js"></i>
                     <i className="fa-brands fa-npm"></i>
@@ -124,7 +133,7 @@ export default function About(){
 
             </div>
 
-            <div className="about-cert">
+            <div data-aos="fade-up" className="about-cert">
 
                 <a className="cert-link" href="https://drive.google.com/drive/folders/11N6U03qG1wlc3Or73msltMCmMV7vEpyG?usp=sharing" target="_blank" rel="noreferrer">
                 <p >Check out my certificates <i class="fa-solid fa-arrow-right"></i></p>
